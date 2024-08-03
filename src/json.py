6697f51ac27dfbb19f749f65ede2c79e5d8d1b28
Ken Saba <kensaba@airmail.cc>
2024-08-04 00:00:55 +0800

    Initial Commit

---
+++ src/json.py
@@ -0,0 +1,10 @@
+from typing import IO
+
+class JSON:
+    def __init__(self):
+        pass
+    def loads(self, s: str) -> dict:
+        pass
+    def load(self, f: IO[str]) -> dict:
+        pass
+
